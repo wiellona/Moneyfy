c548a32b1157ca2660617717f9eeca483a3c6b79
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2025-05-17 10:01:31 +0700

    login register integration, and new header

--- my-project/src/Components/Header.jsx
+++ my-project/src/Components/Header.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import "lucide-react";
+import { UserContext } from "../context/AuthContext";
 
-const Header = () => {
+const Header = ({ user }) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const { logout } = useContext(UserContext);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleLogout = () => {
+    logout();
   };
 
   return (
@@ -27,17 +34,96 @@ const Header = () => {
               Contact Us
             </a>
           </div>
-          <div className="flex gap-4 items-center">
-            <a href="/login" className="text-gray-700 hover:text-indigo-600">
-              Register
-            </a>
-            <a
-              href="/login"
-              className="text-white bg-indigo-600 px-4 py-2 rounded hover:bg-indigo-700"
-            >
-              Login
-            </a>
-          </div>
+
+          {user ? (
+            <div className="flex gap-4 items-center">
+              <button
+                onClick={() =>
+                  document
+                    .getElementById("userDropdown")
+                    .classList.toggle("hidden")
+                }
+                className="flex items-center text-sm font-medium text-gray-900 rounded-full hover:text-blue-600 "
+                type="button"
+              >
+                {user.profileImage ? (
+                  <img
+                    className="w-10 h-10 me-2 rounded-full object-cover"
+                    src={user.profileImage}
+                    alt="user avatar"
+                  />
+                ) : (
+                  <div className="w-10 h-10 me-2 rounded-full bg-indigo-600 flex items-center justify-center text-white text-xl font-bold">
+                    {user.name ? user.name.charAt(0).toUpperCase() : "U"}
+                  </div>
+                )}
+                <span>{user.name || "User"}</span>
+                <svg
+                  className="w-2.5 h-2.5 ms-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
+
+              <div
+                id="userDropdown"
+                className="hidden absolute right-2 top-full bg-white divide-y divide-gray-100 rounded-lg shadow-lg w-48 z-20"
+              >
+                <div className="px-4 py-3 text-sm text-gray-900">
+                  <div className="font-medium">{user.name}</div>
+                  <div className="truncate">{user.email}</div>
+                </div>
+                <ul className="py-2 text-sm text-gray-700">
+                  <li>
+                    <a
+                      href="/dashboard"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                    >
+                      Dashboard
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/settings-transactions"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                    >
+                      Settings
+                    </a>
+                  </li>
+                </ul>
+                <div className="py-2">
+                  <button
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    onClick={handleLogout}
+                  >
+                    Sign out
+                  </button>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="flex gap-4 items-center">
+              <a href="/login" className="text-gray-700 hover:text-indigo-600">
+                Register
+              </a>
+              <a
+                href="/login"
+                className="text-white bg-indigo-600 px-4 py-2 rounded hover:bg-indigo-700"
+              >
+                Login
+              </a>
+            </div>
+          )}
         </div>
 
         {/* Mobile Menu */}
@@ -63,27 +149,107 @@ const Header = () => {
 
         {/* Mobile Menu Dropdown */}
         {isMobileMenuOpen && (
-          <div className="md:hidden bg-white shadow-lg p-4 space-y-4">
-            <a href="#" className="block text-gray-700">
+          <div className="md:hidden  flex flex-col items-start bg-white shadow-lg p-4 space-y-5">
+            {user ? (
+              <div className="flex gap-4 items-center">
+                <button
+                  onClick={() =>
+                    document
+                      .getElementById("userDropdownMobile")
+                      .classList.toggle("hidden")
+                  }
+                  className="flex items-center text-sm font-medium text-gray-900 rounded-full hover:text-blue-600 "
+                  type="button"
+                >
+                  {user.profileImage ? (
+                    <img
+                      className="w-10 h-10 me-2 rounded-full object-cover"
+                      src={user.profileImage}
+                      alt="user avatar"
+                    />
+                  ) : (
+                    <div className="w-10 h-10 me-2 rounded-full bg-indigo-600 flex items-center justify-center text-white text-xl font-bold">
+                      {user.name ? user.name.charAt(0).toUpperCase() : "U"}
+                    </div>
+                  )}
+                  <span>{user.name || "User"}</span>
+                  <svg
+                    className="w-2.5 h-2.5 ms-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 4 4 4-4"
+                    />
+                  </svg>
+                </button>
+
+                <div
+                  id="userDropdownMobile"
+                  className="hidden absolute top-32 bg-white divide-y divide-gray-100 rounded-lg shadow-lg w-48 z-20"
+                >
+                  <div className="px-4 py-3 text-sm text-gray-900">
+                    <div className="font-medium">{user.name}</div>
+                    <div className="truncate">{user.email}</div>
+                  </div>
+                  <ul className="py-2 text-sm text-gray-700">
+                    <li>
+                      <a
+                        href="/dashboard"
+                        className="block px-4 py-2 hover:bg-gray-100"
+                      >
+                        Dashboard
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="/settings-transactions"
+                        className="block px-4 py-2 hover:bg-gray-100"
+                      >
+                        Settings
+                      </a>
+                    </li>
+                  </ul>
+                  <div className="py-2">
+                    <button
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      onClick={handleLogout}
+                    >
+                      Sign out
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="flex gap-4 items-center">
+                <a
+                  href="/login"
+                  className="block text-gray-700 hover:text-indigo-600"
+                >
+                  Register
+                </a>
+                <a
+                  href="/login"
+                  className="block text-white bg-indigo-600 px-4 py-2 rounded hover:bg-indigo-700"
+                >
+                  Login
+                </a>
+              </div>
+            )}
+            <a href="#" className="block text-gray- hover:text-indigo-600">
               About us
             </a>
-            <a href="#" className="block text-gray-700">
+            <a href="#" className="block text-gray- hover:text-indigo-600">
               Our Features
             </a>
-            <a href="#" className="block text-gray-700">
+            <a href="#" className="block text-gray-700 hover:text-indigo-600">
               Contact Us
-            </a>
-            <a
-              href="#"
-              className="block text-white bg-indigo-600 px-4 py-2 rounded hover:bg-indigo-700"
-            >
-              Register
-            </a>
-            <a
-              href="#"
-              className="block text-white bg-indigo-600 px-4 py-2 rounded hover:bg-indigo-700"
-            >
-              Login
             </a>
           </div>
         )}
