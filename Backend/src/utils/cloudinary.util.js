78544b8751d9d832e9b9a76b4bbfe212384b4993
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2025-05-20 22:28:36 +0700

    add change profile pict integration

--- Backend/src/utils/cloudinary.util.js
+++ Backend/src/utils/cloudinary.util.js
@@ -1,7 +1,10 @@
 const cloudinary = require("cloudinary").v2;
 
+// Configure Cloudinary with environment variables
 cloudinary.config({
-  cloudinary_url: process.env.CLOUDINARY_URL,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 /**
